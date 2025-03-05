337493256e749e73d01fb33784715325557e8723
Kun Huang <knhg2013@gmail.com>
2025-03-05 10:57:16 -0500

    Route setup till lec 21 (title is mssing some lecs before)

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <div>About this app</div>
+      <p>App to search Git Hub users</p>
+      <p>Version 1.0.0</p>
+    </div>
+  );
+};
+
+export default About;
