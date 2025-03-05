609cc90481f797964c9fbb36972986ffdf326a25
Kun Huang <knhg2013@gmail.com>
2025-03-05 16:55:22 -0500

    use Gemini to fix the react-router-dom upgrade to V6 and no working issue

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,35 +1,45 @@
-import React, { Component } from "react";
-import { useParams } from "react-router-dom";
+import React, { Component, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-export class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
+const User = ({ user, getUser }) => {
+  //   componentDidMount() {
+  //     this.props.getUser(this.props.match.params.login);
+  //   }
+
+  const { login } = useParams(); // Get the :login parameter from the URL
+
+  useEffect(() => {
+    getUser(login); // Fetch user data using the login parameter
+    // eslint-disable-next-line
+  }, []);
+
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    loading,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
+
+  if (loading) {
+    return <div>Loading...</div>;
+  } else {
+    return <div>User</div>;
   }
+};
 
-  render() {
-    const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable,
-    } = this.props.user;
-
-    const { loading } = this.props;
-
-    if (!loading) {
-      return <div>Loading...</div>;
-    } else {
-      return <div>User</div>;
-    }
-  }
-}
+User.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  user: PropTypes.object.isRequired,
+  getUser: PropTypes.func.isRequired,
+};
 
 export default User;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -35,7 +35,7 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
       ></img>
       <h3>{login}</h3>
       <div>
-        <Link to={`/users/${login}`} className='btn btn-dark btn-sm my-1'>
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
           More
         </Link>
       </div>
